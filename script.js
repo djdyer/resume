68571b1e1ee55c720f69737c92381e45f5764bae
David Dyer <davidrossdyer@gmail.com>
2021-09-15 16:42:18 -0400

    created table, grid for schools, fixed read more button

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // document.getElementById("readMore").addEventListener("click", expandBio());
 
 function expandBio() {
-  var dots = document.getElementById("#dots");
-  var fullBio = document.getElementById("#fullBio");
-  var readMore = document.getElementById("#readMore");
+  var dots = document.getElementById("dots");
+  var fullBio = document.getElementById("fullBio");
+  var readMore = document.getElementById("readMore");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
