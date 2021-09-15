fa1fad11d888ca2a2a3ccfecd88b30d2d0752f08
David Dyer <davidrossdyer@gmail.com>
2021-09-15 12:52:17 -0400

    adding js. many changes to style

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.getElementById("readMore").addEventListener("click", expandBio());
+
+function expandBio() {
+  var dots = document.getElementById("#dots");
+  var fullBio = document.getElementById("#fullBio");
+  var readMore = document.getElementById("#readMore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    readMore.innerHTML = "Read more";
+    fullBio.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    readMore.innerHTML = "Read less";
+    fullBio.style.display = "inline";
+  }
+}
